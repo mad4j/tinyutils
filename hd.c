439126b88922e71da060a412b51103699a1b7872
Daniele Olmisani <mad4j@users.noreply.github.com>
2018-12-12 22:09:22 +0100

    Add files via upload

---
+++ hd.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+    unsigned long count = 0;
+
+    int c = 0;
+    while ((c = getchar()) != EOF) {
+
+        if ((count % 16) == 0) {
+            printf("\n %08lu ", count);
+        }
+
+        printf(" %02x", c);
+
+        count++;
+    }
+
+    printf("\n");
+
+    return count;
+}
